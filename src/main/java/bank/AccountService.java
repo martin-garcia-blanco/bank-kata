df226cc02d6e5ecc4c238fe4fd17b5f3551c1f26
martin-garcia-blanco <programadopormartin@gmail.com>
2020-09-30 11:17:48 +0200

    :sparkles: Create an acceptance test for the entire feature

---
+++ src/main/java/bank/AccountService.java
@@ -0,0 +1,16 @@
+package bank;
+
+public class AccountService {
+
+  public void deposit(int amount) {
+    throw new UnsupportedOperationException();
+  }
+
+  public void withdrawal(int amount) {
+    throw new UnsupportedOperationException();
+  }
+
+  public String printStatement() {
+    throw new UnsupportedOperationException();
+  }
+}
---
+++ src/test/java/acceptance/PrintStatementFeature.java
@@ -0,0 +1,26 @@
+package acceptance;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import bank.AccountService;
+import org.junit.jupiter.api.Test;
+
+public class PrintStatementFeature {
+
+  @Test
+  void be_able_to_create_multiple_transactions_and_print_the_statement() {
+    String expectedOutput = "Date || Amount || Balance\n"
+        + "30/09/2020 || 1000 || 1000\n"
+        + "30/09/2020 || 100 || 1100\n"
+        + "30/09/2020 || -500 || 600\n";
+
+    AccountService accountService = new AccountService();
+    accountService.deposit(1000);
+    accountService.deposit(100);
+    accountService.withdrawal(500);
+
+    String output = accountService.printStatement();
+    assertThat(output, is(expectedOutput));
+  }
+}
