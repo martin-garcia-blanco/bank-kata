8901ab3fdc48587b98b8e42608372c912ad6d303
martin-garcia-blanco <programadopormartin@gmail.com>
2020-09-30 13:00:45 +0200

    :recycle: Refactor equals method, get intellij autogenerated

--- src/main/java/bank/Transaction.java
+++ src/main/java/bank/Transaction.java
@@ -1,5 +1,7 @@
 package bank;
 
+import java.util.Objects;
+
 public class Transaction {
 
   private final String date;
@@ -19,8 +21,20 @@ public class Transaction {
   }
 
   @Override
-  public boolean equals(Object obj) {
-    Transaction transaction = (Transaction)obj;
-    return this.date.equals(transaction.date) && this.amount == transaction.amount;
+  public int hashCode() {
+    return Objects.hash(date, amount);
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Transaction that = (Transaction) o;
+    return amount == that.amount &&
+        Objects.equals(date, that.date);
   }
 }
