1228a52c06fae9a3f10eb9b33acc50c8b3069bca
martin-garcia-blanco <programadopormartin@gmail.com>
2020-09-30 14:41:30 +0200

    :sparkles: Implement logic to add multiple transaction in Transactions

--- src/test/java/unit/TransactionsShould.java
+++ src/test/java/unit/TransactionsShould.java
@@ -17,4 +17,15 @@ public class TransactionsShould {
     transactions.add(new Transaction("30/09/2020", 100));
     assertThat(transactions.transactions.size(), is(1));
   }
+
+  @Test
+  void be_able_to_store_multiple_transaction_items() {
+    Transactions transactions = new Transactions();
+    assertThat(transactions.transactions.size(), is(0));
+    transactions.add(new Transaction("30/09/2020", 100));
+    transactions.add(new Transaction("30/09/2020", -500));
+    assertThat(transactions.transactions.size(), is(2));
+    transactions.add(new Transaction("30/09/2020", 1000));
+    assertThat(transactions.transactions.size(), is(3));
+  }
 }
