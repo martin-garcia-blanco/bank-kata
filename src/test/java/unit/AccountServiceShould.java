b585c5aac503082904b43ac022ed77548536d0c9
martin-garcia-blanco <programadopormartin@gmail.com>
2020-09-30 12:02:19 +0200

    :recycle: Create a setUp method for unit tests

--- src/test/java/unit/AccountServiceShould.java
+++ src/test/java/unit/AccountServiceShould.java
@@ -8,6 +8,7 @@ import static org.mockito.Mockito.verify;
 import bank.AccountService;
 import bank.Transaction;
 import bank.Transactions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -19,34 +20,32 @@ public class AccountServiceShould {
 
   @Mock
   private Transactions transactions;
+  private AccountService accountService;
+
+
+  @BeforeEach
+  void setUp() {
+    accountService = new AccountService(transactions);
+  }
 
   @Test
   void be_able_to_receive_an_deposit_and_store_it() {
-    AccountService accountService = new AccountService(transactions);
     Transaction deposit = new Transaction("30/09/2020", 100);
-
     accountService.deposit(100);
-
     verify(transactions).add(deposit);
   }
 
   @Test
   void be_able_to_receive_a_withdrawal_and_store_it() {
-    AccountService accountService = new AccountService(transactions);
     Transaction withdrawal = new Transaction("30/09/2020", -100);
-
     accountService.withdrawal(100);
-
     verify(transactions).add(withdrawal);
   }
 
   @Test
   void be_able_to_print_the_headers() {
     String expectedOutput = "Date || Amount || Balance";
-
-    AccountService accountService = new AccountService(transactions);
     String output = accountService.printStatement();
-
     assertThat(output, is(expectedOutput));
   }
 
