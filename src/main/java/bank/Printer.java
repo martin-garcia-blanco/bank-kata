79511d59b89bd43bdd966bce233c4bdca17a2368
martin-garcia-blanco <programadopormartin@gmail.com>
2020-09-30 12:44:41 +0200

    :recycle: Extract more logic of build the transaction lines into a new method

--- src/main/java/bank/Printer.java
+++ src/main/java/bank/Printer.java
@@ -7,11 +7,22 @@ public class Printer {
   private final String HEADERS = String.format("%s || %s || %s", "Date", "Amount", "Balance");
 
   public String print(List<Transaction> transactions) {
-    StringBuilder output = new StringBuilder(HEADERS);
-    for (Transaction transaction : transactions) {
-      output.append(buildTransactionLine(transaction));
-    }
+    StringBuilder output = new StringBuilder();
+    output.append(buildHeaders());
+    output.append(buildTransactionLines(transactions));
     return output.toString();
+  }
+
+  private String buildTransactionLines(List<Transaction> transactions) {
+    StringBuilder result = new StringBuilder();
+    for (Transaction transaction : transactions) {
+      result.append(buildTransactionLine(transaction));
+    }
+    return result.toString();
+  }
+
+  private String buildHeaders() {
+    return HEADERS;
   }
 
   private String buildTransactionLine(Transaction transaction) {
